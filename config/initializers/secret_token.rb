4e4d86dc2b7a560da82c7eae6c4cc7e972fdd964
Andrew Tae <mattan@onemonthrails.com>
2016-06-09 21:34:57 -0700

    initial commit for failed project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarks::Application.config.secret_token = '9736e7cb8696c8e91a608caebeb15e666433cb6193c29c7f70436189ba03d20c17e578fc4a68148fa2713f144534670944738dd22acc1bc17d740503583b5b8c'
